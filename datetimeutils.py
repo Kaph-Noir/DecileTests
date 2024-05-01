d09e0fc62baa1c5e6d3f2acd52024640c863412d
Kaph-Noir <i.hack.gene@gmail.com>
2024-05-01 13:47:46 +0900

    Update datetimeutils.py
    get_str_yyyy_mm_dd_next_y
    : leap year condition

--- datetimeutils.py
+++ datetimeutils.py
@@ -8,8 +8,8 @@ class DateTimeUtils:
     Returns:
         _type_: _description_
     """
-    @staticmethod
-    def get_str_yyyy(yyyymmdd: int) -> str:
+
+    def get_str_yyyy(self, yyyymmdd: int) -> str:
         return str(yyyymmdd)[0:4]
     
     @staticmethod
@@ -23,12 +23,16 @@ class DateTimeUtils:
     @staticmethod
     def get_int_yyyymm(yyyymmdd: int) -> int:
         return int(str(yyyymmdd)[0:6])  # yyyymmdd // 100
+    
+    def get_str_yyyymmdd(self, yyyymmdd: int) -> str:
+        return str(yyyymmdd)
 
     @staticmethod
     def get_str_yyyy_mm_dd(yyyymmdd: int) -> str:
         return dt.strftime(dt.strptime(str(yyyymmdd), "%Y%m%d"), "%Y-%m-%d")
 
-    @staticmethod
-    def get_str_yyyy_mm_dd_next_y(yyyymmdd: int) -> str:
-        return dt.strftime(dt.strptime(str(yyyymmdd), "%Y%m%d") + timedelta(days=365), "%Y-%m-%d")
-
+    def get_str_yyyy_mm_dd_next_y(self, yyyymmdd: int) -> str:
+        if self.get_int_yyyy(self.get_str_yyyy(yyyymmdd)) % 4 == 3:
+            return dt.strftime(dt.strptime(self.get_str_yyyymmdd(yyyymmdd), "%Y%m%d") + timedelta(days=365), "%Y-%m-%d")
+        else:
+            return dt.strftime(dt.strptime(self.get_str_yyyymmdd(yyyymmdd), "%Y%m%d") + timedelta(days=364), "%Y-%m-%d")
